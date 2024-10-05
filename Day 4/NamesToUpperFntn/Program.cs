01766192bafba3c18317cddefe3b958ff6059714
Sonica-R <134711681+Sonica-R@users.noreply.github.com>
2024-10-05 17:29:29 +0530

    Initial commit for Day 4

---
+++ Day 4/NamesToUpperFntn/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamesToUpperFntn
+{
+    internal class Program
+    {
+        static void NamesToUpper(string[] names)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            
+        }
+        static void ArrayCheck(string[] names,string check)
+        {
+            Array.Contains(names.check, StringComparison.OrdinalIgnoreCase);
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number of names :");
+            string[] names = new string[n];
+            Console.WriteLine("Enter the names");
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+            NamesToUpper(names);
+            Console.WriteLine("Enter a name to be checked");
+            string check = Console.ReadLine();
+            ArrayCheck(check);
+
+        }
+    }
+}
---
+++ Day 4/NumToString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new string[] { "one", "two", "three", "four", "five", "six", "seven" };
+            //Console.Write("Enter the length of array : ");
+            string[] input = new string[] { "One, 2, Three, 2" };
+
+            var inpItems = input.Replace(" ", "").Split(',');
+
+            for (int j = 0; j < arrNum.Length; j++)
+            {
+                Console.WriteLine(arrNum[j]);
+            }
+
+        }
+    }
+}
