7ed9ffcdd84a8f5664c1b7dae8f7b5df437479f1
Sonica-R <134711681+Sonica-R@users.noreply.github.com>
2024-10-06 16:03:41 +0530

    Commit for Day 1 and Day 3

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "10_200_3_1";
+            int sum = 0;
+            string str = "";
+            for (int i = 0; i < data.Length; i++)
+            {
+                //char ch = data[i];
+                if(data[i] != '_')
+                {
+                    str += data[i];
+                }        
+                else
+                {
+                    sum += int.Parse(str);
+                    str = "";
+                }
+                if (i == data.Length - 1)
+                {
+                    sum += int.Parse(str);
+                }
+
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Assignment1/PrintPattern/PrintPattern/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the height of the rectangle");
+            int height = int.Parse(Console.ReadLine());
+            int length = 12;
+
+            for (int i = 0; i < height; i++)
+            {
+                if ((i == 0) || (i == height - 1))
+                {
+                    Console.Write("*************");
+                }
+                for (int j = 0; j < length; j++)
+                {
+                    if (!((i == 0) || (i == height - 1)))
+                    {
+                        if ((j == 0) || (j == length - 1))
+                        {
+                            Console.Write("* ");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
